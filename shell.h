630e0eb6ac11c8eee0ea0bedc33e3eb6ff9d6e92
Seb1776 <sebastianwho1776@gmail.com>
2021-04-07 20:54:52 +0000

    Added header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef S_SHELL
+#define S_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#endif
