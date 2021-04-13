f497e83f030121ce706d002b2a4b50635e424043
F-balanta <jf.balanta@hotmail.com>
2021-04-13 01:14:43 +0000

    Version proof

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * main - This function is the main function and execute other functions
+ * Return:int
+ */
+int main(void)
+{
+
+	char *linea, **token;
+	int status;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		linea = read_Getline();
+		token = strtok_line(linea);
+		status = execute(token);
+		free(linea);
+		free(token);
+	}
+	return (0);
+}
