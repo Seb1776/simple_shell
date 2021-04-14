bf83e4adb8a3942155c43ceb19dd20892f8961b0
F-balanta <jf.balanta@hotmail.com>
2021-04-14 22:57:43 +0000

    main.c overwriten

--- main.c
+++ main.c
@@ -3,36 +3,51 @@
 /**
  * main - Main function, initializes Shell
  *
- * @argc: Amount of Arguments
- * @argv: Arguments
+ * @ac: Amount of Arguments
+ * @av: Arguments
  *
  * Return: current state
  */
-int main(void)
+int main(int ac, char **av)
 {
-	int cmd;
-	char *dspl = "$ ", *empty = NULL, **token = NULL;
-	size_t n;
+	char *display = "$ ", *empty = NULL, **arr;
+	int i, cmd, hst;
+	size_t n = 0;
+	struct stat st;
 
-
+	if (ac <= 1)
+		hsh_call = av[0];
+	global_st = 0;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, dspl, strlen(dspl));
-
+			write(STDOUT_FILENO, display, _strlen(display));
 		cmd = getline(&empty, &n, stdin);
-		token = strtok_line(empty);
-		hsh_execute(token);
 		if (cmd == EOF)
 		{
 			if (isatty(STDIN_FILENO))
 				_putchar('\n');
-			invoke_sh_exit(empty);
+			hsh_shell_exit(empty);
 		}
 		if (cmd != -1)
 		{
-			builtin_manager(empty);
+			if (builtin_manager(empty) == 0)
+			{
+				arr = hsh_shell_manage_path(empty);
+				if (stat(arr[0], &st) == 0)
+					hsh_execute(arr, hst, empty);
+				else
+					error_manager(empty, hst);
+				for (i = 0; arr[i]; i++)
+					free(arr[i]);
+				free(arr);
+			}
 		}
+		else
+			error_manager(empty, hst);
+		hst++;
 	}
+	free(empty);
+	return (global_st);
 }
