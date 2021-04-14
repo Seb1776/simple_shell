f9fbf77d232ce39406726862c4c8f7cb47b85910
Seb1776 <sebastianwho1776@gmail.com>
2021-04-14 16:16:25 +0000

    Added header file

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef S_SHELL
-#define S_SHELL
+#ifndef OWN_SHELL
+#define OWN_SHELL
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,23 +25,30 @@ typedef struct builtins
 	int (*func)(char *);
 } builtin_funcs;
 
-int global_st;
-char *hsh_call;
 extern char **environ;
+char *hsh_call;
+int global_st;
 
-int _putchar(char c);
-int invoke_sh_exit(char *cmd);
-int invoke_sh_printenv(char *cmd);
+void hsh_execute(char **cmd, int hst, char *);
+int hsh_shell_exit(char *cmd);
+int _strlen(char *s);
+int hsh_shell_printenv(char *cmd);
 int builtin_manager(char *cmd);
 void hsh_error_type_a(char *cmd, int hst);
 void hsh_error_type_b(char *cmd, int hst);
 void error_manager(char *cmd, int hst);
+char **hsh_shell_manage_path(char *cmd);
+char **write_to_buffer(char *cmd, char *delims);
+char *hsh_shell_freshpath(char *pathv);
+int check_for_empty(char *env);
 int _strcmp(char *frm, const char *dst, int size);
-void _atoi(int i);
-void _puts(char *str);
+char *_getenv(char *nam);
+char *_strdup(char *str);
+int _putchar(char c);
 int _puterror(char c);
-int _strlen(char *s);
-int check_for_empty(char *str);
-char **strtok_line(char *line);
-void hsh_execute(char **cmd);
+void _puts(char *str);
+char *_strcat(char *str1, char *str2);
+int set_delimit(char *cmd, char *delims);
+void _atoi(int i);
+
 #endif
